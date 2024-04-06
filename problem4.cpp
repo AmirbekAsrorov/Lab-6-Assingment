98f7390f5ec96cc3ec06c574207669a6deba8d1b
AmirbekAsrorov <147371906+AmirbekAsrorov@users.noreply.github.com>
2024-04-06 21:37:25 +0500

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -4,47 +4,37 @@
 
 using namespace std;
 
-void findMinAbsDiffPairs(vector<int>& arr, int index, int minDiff, vector<vector<int>>& pairs) {
-    if (index >= arr.size()) {
+void findMinAbsDiffPairs(vector<int>& arr, int index, int minDiff, vector<vector<int>>& pairs){
+    if(index>=arr.size()){
         return;
     }
-
-    if (index > 0) {
-        int diff = arr[index] - arr[index - 1];
-
-        if (diff < minDiff) {
-            minDiff = diff;
+    if (index>0){
+        int diff=arr[index]-arr[index-1];
+        if(diff<minDiff){
+            minDiff=diff;
             pairs.clear();
-            pairs.push_back({arr[index - 1], arr[index]});
-        } else if (diff == minDiff) {
-            pairs.push_back({arr[index - 1], arr[index]});
+            pairs.push_back({arr[index-1], arr[index]});
+        } else if(diff==minDiff){
+            pairs.push_back({arr[index-1], arr[index]});
         }
     }
-
-    findMinAbsDiffPairs(arr, index + 1, minDiff, pairs);
+    findMinAbsDiffPairs(arr, index+1, minDiff, pairs);
 }
-
-int main() {
+int main(){
     int n;
-    cout << "Enter the size of the array: ";
-    cin >> n;
-
+    cout<<"Enter the size of the array: ";
+    cin>>n;
     vector<int> arr(n);
-
-    cout << "Enter the elements of the array:" << endl;
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout<<"Enter the elements of the array:"<<endl;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
-
     sort(arr.begin(), arr.end());
-
     vector<vector<int>> minAbsDiffPairs;
     findMinAbsDiffPairs(arr, 1, arr[1] - arr[0], minAbsDiffPairs);
-
-    cout << "Pairs with minimum absolute difference:" << endl;
+    //cout << "Pairs with minimum absolute difference:" << endl;
     for (const vector<int>& pair : minAbsDiffPairs) {
         cout  << pair[0] << " " << pair[1]  << endl;
     }
-
     return 0;
 }
